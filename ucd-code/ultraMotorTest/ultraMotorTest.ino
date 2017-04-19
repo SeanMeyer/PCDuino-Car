5830b53d6c121fffd5d232eaff265c4889726973
meyersean <nimblerabit@gmail.com>
2017-04-19 10:32:50 -0600

    Add a reset sketch

--- ucd-code/ultraMotorTest/ultraMotorTest.ino
+++ ucd-code/ultraMotorTest/ultraMotorTest.ino
@@ -3,22 +3,52 @@
 const int trig = 1;
 const int echo = 2;
 
-#define pinI1 8
+#define pinI1 8		// pins 1 + 2 is the right motor. 1 low, 2 high is forwards.
 #define pinI2 11
-#define pinI3 12
+#define pinI3 12	// pins 3 + 4 is the left motor.  3 low, 4 high is forwards.
 #define pinI4 13
 #define speedpinA 9
 #define speedpinB 10
-#define speed 200
+#define speed 25
 
 void forward()
 {
 	analogWrite(speedpinA, speed);
 	analogWrite(speedpinB, speed);
-	digitalWrite(pinI4, HIGH);
-	digitalWrite(pinI3, LOW);
+	digitalWrite(pinI4, LOW);
+	digitalWrite(pinI3, HIGH);
 	digitalWrite(pinI2, LOW);
 	digitalWrite(pinI1, HIGH);
+}
+
+void backward()
+{
+	analogWrite(speedpinA, speed);
+	analogWrite(speedpinB, speed);
+	digitalWrite(pinI4, HIGH);
+	digitalWrite(pinI3, LOW);
+	digitalWrite(pinI2, HIGH);
+	digitalWrite(pinI1, LOW);
+}
+
+void left()
+{
+	analogWrite(speedpinA, speed);
+	analogWrite(speedpinB, speed);
+	digitalWrite(pinI1, LOW);
+	digitalWrite(pinI2, HIGH);
+	digitalWrite(pinI4, LOW);
+	digitalWrite(pinI3, HIGH);
+}
+
+void right()
+{
+	analogWrite(speedpinA, speed);
+	analogWrite(speedpinB, speed);
+	digitalWrite(pinI1, HIGH);
+	digitalWrite(pinI2, LOW);
+	digitalWrite(pinI4, HIGH);
+	digitalWrite(pinI3, LOW);
 }
 
 long microsecondsToInches(long microseconds)
@@ -55,6 +85,12 @@ void loop()
 	inches = microsecondsToInches(duration);
 	cm = microsecondsToCentimeters(duration);
 	printf("%d in, %d cm \n", inches, cm);
-	delay(80);
 	forward();
+	delay(1000);
+	backward();
+	delay(1000);
+	left();
+	delay(1000);
+	right();
+	delay(1000);
 }
