9ac0e87da41ffea1dcb7b3060dbb207a7833503f
Ameen Sassi <ameensassi@gmail.com>
2017-04-24 12:13:31 -0600

    Completed Adjacency List Program
    Works and creates adjacency list based on text file.

--- MapCreation-Search code/Adjacency List.py
+++ MapCreation-Search code/Adjacency List.py
@@ -9,6 +9,9 @@ class Node:
     def add_neighbor(self, neighbor):
         self.connectedTo[neighbor] = 1
 
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
     def get_connections(self):
         return self.connectedTo.keys()
 
@@ -41,10 +44,10 @@ class Map:
 
     def add_road(self, i, j):
         if i not in self.nodeList:
-            n = self.add_nodes(i)
+            self.add_nodes(i)
         if j not in self.nodeList:
-            n = self.add_nodes(j)
-        self.nodeList[i].addNeighbor(self.nodeList[j])
+            self.add_nodes(j)
+        self.nodeList[i].add_neighbor(self.nodeList[j])
 
     def get_nodes(self):
         return self.nodeList.keys()
@@ -54,14 +57,19 @@ class Map:
 
 
 def main(argv):
-    with open("Embedded_Map.txt", "r") as map:
-        for line in map:
-            data_list = line,
-            print data_list
+    graph = Map()
+    with open("insert file path to Embedded_Map.txt here", "r") as mapp:
+        for line in mapp:
+            data_list = line.split(';')
+            node_main = str(data_list[0][0] + data_list[0][1])
+            graph.add_nodes(node_main)
+            for i in range(len(data_list)-1):
+                node_sub = str(data_list[i+1][0] + data_list[i+1][1])
+                graph.add_road(node_main, node_sub)
     pass
 
 
 if __name__ == '__main__':
     main(sys.argv)
 
-
+#Thanks to http://interactivepython.org/courselib/static/pythonds/Graphs/Implementation.html for implementation of Adjacency List
