ce8487fb7f7bed0ab0f9c1adf71a886703882eaa
meyersean <nimblerabit@gmail.com>
2017-05-01 15:14:13 -0600

    Current driving

--- ucd-code/driving/driving.ino
+++ ucd-code/driving/driving.ino
@@ -5,8 +5,8 @@ extern "C" {
   #include <inttypes.h>
 }
 #include "Adafruit_VL6180X.h"
-
 #include <core.h>
+#include <math.h>
 
 const int trig = 3;
 const int echo = 4;
@@ -18,13 +18,26 @@ const int echo = 4;
 #define speedpinA 9
 #define speedpinB 10
 
-#define speed 25
+#define speed 30
+int leftSpeed = speed;
+int rightSpeed = speed;
 
 Adafruit_VL6180X laser1;
 byte laser1SHDNPin = 1;
 
 Adafruit_VL6180X laser2;
 byte laser2SHDNPin = 7;
+
+int getMotor(char motor) {
+  switch(motor) {
+    case 'L':
+    case 'l':
+      return leftSpeed;
+    case 'R':
+    case 'r':
+      return rightSpeed;
+  }
+}
 
 void forward()
 {
@@ -36,6 +49,24 @@ void forward()
   digitalWrite(pinI1, HIGH);
 }
 
+
+void setMotor(char motor, int newSpeed)
+{
+  switch(motor) {
+    case 'L':
+    case 'l':
+      analogWrite(speedpinB, newSpeed);
+      leftSpeed = newSpeed;
+      break;
+    case 'R':
+    case 'r':
+      analogWrite(speedpinA, newSpeed);
+      rightSpeed = newSpeed;
+      break;
+  }
+  return;
+}
+
 void startCar()
 {
   forward();
@@ -43,23 +74,8 @@ void startCar()
 
 void stopCar()
 {
-  digitalWrite(speedpinA, LOW);
-  digitalWrite(speedpinB, LOW);
-}
-
-void setMotor(char motor, int newSpeed)
-{
-  switch(motor) {
-    case 'L':
-    case 'l':
-      analogWrite(speedpinA, newSpeed);
-      break;
-    case 'R':
-    case 'r':
-      analogWrite(speedpinB, newSpeed);
-      break;
-  }
-  return;
+  setMotor('L', 0);
+  setMotor('R', 0);
 }
 
 void backward()
@@ -132,6 +148,56 @@ int getDistance(char laser)
   }
 }
 
+double difference(int left, int right) {
+    double total = left + right;
+    int sub;
+    if (left == right) {
+        return 0;
+    } else if (left >= right) {
+        sub = left - right;
+    } else {
+        sub = right - left;
+    }
+    return ((double) sub / total);
+}
+
+void driveStraight() {
+    double percentDif;
+    int leftLaser = getDistance('l');
+    int rightLaser = getDistance('r');
+    percentDif = difference(leftLaser, rightLaser);
+    char motor1 = 'l';
+    char motor2 = 'r';
+    if (leftLaser > rightLaser) {
+        motor1 = 'r';
+        motor2 = 'l';
+    }
+
+    //Stop if no wall
+    if (leftLaser == 25 || rightLaser == 25) {
+      stopCar();
+      while(1) {
+        delay(50);
+      }
+    }
+
+    //Limit motor changes
+    if (getMotor(motor1) >= 90) {
+      setMotor(motor1, 89);
+    } else if (getMotor(motor2) >= 90) {
+      setMotor(motor2, 89);
+    }
+    //Drive straight
+    else {
+      if (percentDif > 0.25) {
+        setMotor( motor1, (int) round(getMotor(motor1)*(1 + (percentDif / 2))));
+        setMotor( motor2, (int) round(getMotor(motor2)*(1 - (percentDif / 2))));  
+      } else {
+        startCar();
+      }
+    }
+}
+
 void setup() {
   printf("starting");
 
@@ -160,12 +226,19 @@ void setup() {
 
   digitalWrite(laser2SHDNPin, HIGH);
   laser2.begin(0x86);
+
+  startCar();
 }
 
 void loop() {
-  printf("looping \n");
-  printf("Left: %d\n", getDistance('L'));
-  printf("Right: %d\n", getDistance('R'));
-  printf("Front: %d\n", getDistance('F'));
-  delay(500);
+  printf("Left: %d  Right: %d,  Front: %d\n\n", getDistance('L'), getDistance('R'), getDistance('F'));
+  printf("          Motor Left: %d   Motor Right: %d\n\n", getMotor('L'), getMotor('R'));
+  if (getDistance('f') <= 20) {
+    printf("Stopping Motors\n");
+    stopCar();
+    return;
+  } else {
+    driveStraight();
+  }
+  delay(100);
 }
